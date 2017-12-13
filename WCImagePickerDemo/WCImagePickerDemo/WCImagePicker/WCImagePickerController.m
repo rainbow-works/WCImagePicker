c95a6adba74675ee67883936679ab671d8b4d5c9
wangchao <504615732@qq.com>
2017-12-13 18:37:49 +0800

    微调

--- WCImagePickerDemo/WCImagePickerDemo/WCImagePicker/WCImagePickerController.m
+++ WCImagePickerDemo/WCImagePickerDemo/WCImagePicker/WCImagePickerController.m
@@ -14,26 +14,27 @@
 
 @implementation WCImagePickerController
 
+- (instancetype)init {
+    if (self = [super init]) {
+        _mediaType = WCImagePickerImageTypeImage;
+        _allowMultipleSelections = YES;
+        _maximumNumberOfSelections = 1;
+        _minimumNumberOfSelections = 1;
+        _numberOfColumnsInPortrait = 4;
+        _numberOfColumnsInLandscape = 7;
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    UINavigationController
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
